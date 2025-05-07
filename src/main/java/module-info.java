8f4f629f0bad51106de4ff48b3a6cc45f090130e
vanessabrenner <vanessabrenner23@gmail.com>
2025-05-08 01:54:19 +0300

    lab3-4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module tasks {
     requires log4j;
 
 //    opens tasks.model to javafx.base;
-    opens tasks.model
+    opens tasks.model;
     exports tasks.model;
     opens tasks.view to javafx.fxml;
     exports tasks.view;
